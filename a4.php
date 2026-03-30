758cb1a55dca48315e60ec53255735e790870787
B292863 <b292863.2025@gmail.com>
2026-03-31 00:02:00 +0100

    update

--- a4.php
+++ a4.php
@@ -21,18 +21,54 @@ echo <<<HEAD_
 </div>
 HEAD_;
 
-// Reference: https://www.w3schools.com/tags/tag_meta.asp
-//
-if (!file_exists($tree)) {
-	echo "<div class='content'>";
-	echo '<meta http-equiv="refresh" content="30">';
-	echo "</div";
-} else {
-	echo "<div class='content'>";
-	echo '<img src="get_phylo_tree.php" alt="Gene Tree" width="500" height="600" class="center">';
-	echo "</div>";
+// Make PDO connection with the database
+if (!$data) {
+        echo "<p align='center'>No data has been selected yet!</p>";
+        die();
 }
 
+// Making the connection
+try {
+        $pdo = new PDO(
+                "mysql:host=$hostname;dbname=$database",
+                $username,
+                $password,
+                [
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                ]
+        );
+
+} catch (PDOException $e) {
+        die("Database error " . $e->getMessage());
+}
+
+// Getting the data
+$query = "SELECT id,organism FROM $data";
+$stmt = $pdo->query($query);
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Print the phylogenetic tree
+// Reference: https://www.w3schools.com/tags/tag_meta.asp
+echo "<div class='content'>";
+if (!file_exists($tree)) {
+	echo '<meta http-equiv="refresh" content="30">';
+} else {
+	echo '<img src="get_phylo_tree.php" alt="Gene Tree" width="500" height="600" class="center">';
+}
+
+// Return list of Organisms and IDs
+echo "<div class='data'>";
+echo "<table>";
+echo "<tr><th>ID</th><th>Organism</th></tr>";
+// Reference: https://www.w3schools.com/php/php_looping_foreach.asp
+// Reference: https://www.ibm.com/docs/en/db2/11.5.x?topic=rqrs-fetching-rows-columns-from-result-sets
+foreach ($rows as $row) {
+	echo "<tr><td>{$row['id']}</td><td>{$row['organism']}</td></tr>";
+}
+echo "</table>";
+echo "</div>";
+echo "</div";
 echo <<<END_
 </html>
 </body>
