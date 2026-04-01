19afb4cbe84eaaebf1e81fe94e3497789f972c7a
B292863 <b292863.2025@gmail.com>
2026-04-01 21:57:31 +0100

    update

--- data_choice.php
+++ data_choice.php
@@ -6,33 +6,30 @@ require_once 'login.php';
 // Saves POST actions as SESSION variables to be used throughout the website
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	// If the user did not select both required inputs, return a message to the screen
 	if (!isset($_POST['family']) || isset($_POST['protein'])) {
 		header("Location: home.php");
 		$_SESSION['message1'] = 'Please specify both a family and a protein';
 	}
+	// If the user created a search, save the search terms into session variables
 	if (isset($_POST['family']) && $_POST['family'] != '' && isset($_POST['protein']) && $_POST['protein'] != '') {
 		$_SESSION['selection'] = 'search';
 		$_SESSION['family'] = $_POST['family'];
 		$_SESSION['protein'] = $_POST['protein'];
 		header("Location: search.php");
-		// Begin executing the tree code
-		// exec("php phylo_tree.php > /dev/null 2>&1 &");
 		exit();
 	} 
+	// If the user selected one of the previous search options, that data selection is saved
 	if (isset($_POST['old_search'])) {
 		$_SESSION['selection'] = 'old';
 		header("Location: home.php");
-		// exec("php phylo_tree.php > /dev/null 2>&1 &");
 		exit();
 	}
+	// If the user chose the example data, that data selection is saved
 	if (isset($_POST['example_data'])) {
 		$_SESSION['selection'] = 'example';
 		header("Location: home.php");
-		// exec("php phylo_tree.php > /dev/null 2>&1 &");
 		exit();
 	}
 }
-
-// header("Location: home.php");
-// exit();
 ?>
